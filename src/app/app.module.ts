3293b8dcc92938d789ba25ef561159815654840a
pasin notebook <pasin.sjr@gmail.com>
2019-05-30 13:08:47 +0700

    initializing application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+import { Component4Component } from './component3/component4/component4.component';
+import { Component5Component } from './component3/component5/component5.component';
+import { Component6Component } from './component3/component6/component6.component';
+import { PopupComponent } from './popup/popup.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, Component1Component, Component2Component, Component3Component, Component4Component, Component5Component, Component6Component, PopupComponent, ModalComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
